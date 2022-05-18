13d79d83a748245ceb04106c3d7bbd9350599b50
CommanderRedYT <ccommanderred@gmail.com>
2022-05-18 12:35:33 +0200

    Moved into seperate files

--- include/led.h
+++ include/led.h
@@ -12,41 +12,11 @@ using ledstrip_t = NeoPixelBrightnessBus<NeoGrbFeature, NeoEsp8266AsyncUart1800K
 
 extern ledstrip_t strip;
 
-void fadeToBlackBy(ledstrip_t &strip, uint8_t fadeBy)
-{
-    for (uint16_t i = 0; i < strip.PixelCount(); i++)
-    {
-        strip.GetPixelColor(i).Darken(fadeBy);
-    }
-}
-
-void fill_solid(ledstrip_t &strip, uint16_t numToFill, RgbColor color)
-{
-    for (uint16_t i = 0; i < std::min(strip.PixelCount(), numToFill); i++)
-    {
-        strip.SetPixelColor(i, color);
-    }
-}
-
-void fill_solid(ledstrip_t &strip, RgbColor color)
-{
-    fill_solid(strip, strip.PixelCount(), color);
-}
-
-RgbColor toNeoPixelColor(CRGB color)
-{
-    return RgbColor{color.r, color.g, color.b};
-}
-
-void fill_rainbow(ledstrip_t &strip, uint16_t startPixel, uint16_t endPixel, uint8_t initialhue, uint8_t deltahue = 1)
-{
-    uint8_t hue = initialhue;
-    for (uint16_t i = startPixel; i < endPixel; i++)
-    {
-        strip.SetPixelColor(i, toNeoPixelColor(CHSV(hue, 255, 255)));
-        hue += deltahue;
-    }
-}
+void fadeToBlackBy(ledstrip_t &strip, uint8_t fadeBy);
+void fill_solid(ledstrip_t &strip, uint16_t numToFill, RgbColor color);
+void fill_solid(ledstrip_t &strip, RgbColor color);
+RgbColor toNeoPixelColor(CRGB color);
+void fill_rainbow(ledstrip_t &strip, uint16_t startPixel, uint16_t endPixel, uint8_t initialhue, uint8_t deltahue = 1);
 
 typedef void (*animation_t)();
 typedef struct {
