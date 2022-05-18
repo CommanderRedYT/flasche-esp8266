ab7620cd78383eae878c9780efafff86eca7f079
CommanderRedYT <ccommanderred@gmail.com>
2022-05-18 19:35:45 +0200

    Added animations page

--- include/html.h
+++ include/html.h
@@ -11,15 +11,18 @@
     "    <title>ESP8266 LED Ring</title>"\
     "</head>"\
     "<style>"\
-    "    footer {"\
+    "    footer {{"\
     "        bottom: 0;"\
     "        position: fixed;"\
     "        width: 100%;"\
     "        padding-bottom: 10px;"\
-    "    }"\
-    "    html {"\
+    "    }}"\
+    "    html {{"\
     "        font-family: Arial, Helvetica, sans-serif;"\
-    "    }"\
+    "    }}"\
+    "    #home {{"\
+    "        margin: 10px;"\
+    "    }}"\
     "</style>" \
     "<body>" \
     x \
@@ -28,11 +31,12 @@
         "<a href=\"https://github.com/bobbycar-graz/flasche-esp8266\">View on GitHub</a> | " \
         "Special thanks to <a href=\"https://github.com/CommanderRedYT\">CommanderRedYT</a> for programming the ESP8266 and " \
         "<a href=\"https://github.com/mickdermack\">mickdermack</a> for providing animations and fixing the code!" \
+        "<a href=\"/\" id=\"home\">Go Back to Main Page</a>" \
     "</footer>" \
     "</body></html>"
 
 namespace html {
-const static char *index_html PROGMEM = HTML_CODE(R"=====(
+const static char* index_html PROGMEM = HTML_CODE(R"=====(
     <h1>ESP8266 LED Ring</h1>
     <h3>Thanks for using this project!</h3>
     <p>
@@ -43,10 +47,13 @@ const static char *index_html PROGMEM = HTML_CODE(R"=====(
         <li>
             <a href="/settings">Settings</a>
         </li>
+        <li>
+            <a href="/animations">Animations</a>
+        </li>
     </ul>
 )=====");
 
-const static char *settings_html PROGMEM = HTML_CODE(R"=====(
+const static char* settings_html PROGMEM = HTML_CODE(R"=====(
     <h1>Settings</h1>
     <p>
         Here you can change the settings of the LED ring.
@@ -54,34 +61,34 @@ const static char *settings_html PROGMEM = HTML_CODE(R"=====(
     <hr>
     <form action="/brightness">
         <label for="brightness">Brightness:</label>
-        <input type="number" name="brightness" value="%d" min="0" max="255" step="1" id="brightness" />
+        <input type="number" name="brightness" value="{}" min="0" max="255" step="1" id="brightness" />
         <input type="submit" value="Save" />
     </form>
     <form action="/animation">
         <label for="animation">Animation:</label>
-        <input type="number" name="animation" value="%d" min="0" max="%d" step="1" id="animation" />
+        <input type="number" name="animation" value="{}" min="0" max="{}" step="1" id="animation" />
         <input type="submit" value="Save" />
     </form>
     <hr>
     <form action="/sta_ssid">
         <label for="sta_ssid">WiFi SSID:</label>
-        <input type="text" name="sta_ssid" value="%s" id="sta_ssid" />
+        <input type="text" name="sta_ssid" value="{}" id="sta_ssid" />
         <input type="submit" value="Save" />
     </form>
     <form action="/sta_password">
         <label for="sta_password">WiFi Password:</label>
-        <input type="text" name="sta_password" value="%s" id="sta_password" />
+        <input type="text" name="sta_password" value="{}" id="sta_password" />
         <input type="submit" value="Save" />
     </form>
     <hr>
     <form action="/ap_ssid">
         <label for="ap_ssid">WiFi AP SSID:</label>
-        <input type="text" name="ap_ssid" value="%s" id="ap_ssid" />
+        <input type="text" name="ap_ssid" value="{}" id="ap_ssid" />
         <input type="submit" value="Save" />
     </form>
     <form action="/ap_password">
         <label for="ap_password">WiFi AP Password:</label>
-        <input type="text" name="ap_password" value="%s" id="ap_password" />
+        <input type="text" name="ap_password" value="{}" id="ap_password" />
         <input type="submit" value="Save" />
     </form>
     <hr>
@@ -92,7 +99,21 @@ const static char *settings_html PROGMEM = HTML_CODE(R"=====(
     </form>
     <form action="/update_url">
         <label for="update_url">Default OTA URL:</label>
-        <input type="text" name="update_url" placeholder="Enter default OTA URL (http) here" value="%s" id="update_url" />
+        <input type="text" name="update_url" placeholder="Enter default OTA URL (http) here" value="{}" id="update_url" />
+        <input type="submit" value="Save" />
+    </form>
+)=====");
+
+const static char* animations_html PROGMEM = HTML_CODE(R"=====(
+    <h1>Animations</h1>
+    <p>
+        Here you can change the animations of the LED ring.
+    </p>
+    <hr>
+    <form action="/animation" id="animation-form">
+        <select name="animation" id="animation" form="animation-form">
+        {}
+        </select>
         <input type="submit" value="Save" />
     </form>
 )=====");
